2a4bba51d8b563247a6b803db23d44a90c76df41
Patrik Molnar <info@patrikmolnar.com>
2019-07-27 13:18:27 +0700

    setup gatsby-node / generate home and product pages

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -1,21 +1,103 @@
-exports.createPages = ({ actions, reporter }) => {
-  reporter.warn("make sure to load data from somewhere!")
+exports.createPages = async ({ actions, graphql }) => {
+  const result = await graphql(`
+    query {
+      brikl {
+        shop(id: "${process.env.GATSBY_BRIKL_SHOP_ID}"){
+          id
+          languages
+          languageWithoutUrlPrefix
+          products(shopId:"${process.env.GATSBY_BRIKL_SHOP_ID}"){
+            edges{
+              node{
+                id
+                no
+                title {
+                  id
+                  text {
+                    content
+                    langCode
+                  }
+                }
+                slugs{
+                  content
+                  langCode
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
 
-  // TODO replace this with data from somewhere
+  const shop = result.data.brikl.shop
+
+  const basePath = "/"
   actions.createPage({
-    path: "/",
-    component: require.resolve("./src/templates/page.js"),
+    path: basePath,
+    component: require.resolve("./src/templates/shop.js"),
     context: {
-      heading: "Your Theme Here",
-      content: `
-        <p>
-          Use this handy theme example as the basis for your own amazing theme!
-        </p>
-        <p>
-          For more information, see 
-          <a href="https://themejam.gatsbyjs.org">themejam.gatsbyjs.org</a>.
-        </p>
-      `,
+      shop,
     },
   })
+
+  try {
+    const languageWithoutUrlPrefix = shop.languageWithoutUrlPrefix
+
+    shop.languages.forEach(language => {
+      const urlPrefix = setUrlPrefix(languageWithoutUrlPrefix, language)
+
+      actions.createPage({
+        path: basePath + urlPrefix,
+        component: require.resolve("./src/templates/shop.js"),
+        context: {
+          shop,
+        },
+      })
+
+      const products = shop.products.edges
+
+      products.forEach(productNode => {
+        const product = productNode.node
+        if (product.slugs) {
+          if (
+            product.slugs &&
+            product.slugs.find(e => e.langCode === language)
+          ) {
+            actions.createPage({
+              path: `/${urlPrefix}${
+                product.slugs.find(e => e.langCode === language).content
+              }`,
+              component: require.resolve("./src/templates/product.js"),
+              context: {
+                langCode: language,
+                slugs: product.slugs,
+                product: product,
+                urlPrefix,
+                shop,
+                productId: product.id,
+              },
+            })
+          }
+        }
+      })
+    })
+  } catch (error) {
+    console.log("error", error)
+  }
+}
+
+const setUrlPrefix = (languageWithoutUrlPrefix, language) => {
+  try {
+    if (languageWithoutUrlPrefix && languageWithoutUrlPrefix === language) {
+      // console.log('no prefix')
+      return ""
+    } else {
+      // console.log('language.substring(0,2)', language.substring(0,2)+'/')
+      return language.substring(0, 2) + "/"
+    }
+  } catch (error) {
+    console.log("Error setting url prefix")
+    return ""
+  }
 }
