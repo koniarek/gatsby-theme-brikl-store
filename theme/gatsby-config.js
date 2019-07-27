d406e07082307d471e42b5cd9ba0f90e03b2dbf8
Patrik Molnar <info@patrikmolnar.com>
2019-07-27 12:23:17 +0700

    setup gatsby-config

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,6 +1,51 @@
+const env = require("dotenv").config()
+const fs = require("fs")
+const { buildSchema } = require("graphql")
+
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Theme Jam Example Submission",
-  },
-  plugins: ["gatsby-plugin-theme-ui"],
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Brikl",
+        fieldName: "brikl",
+        // Url to query from
+        url: process.env.GATSBY_BRIKL_API_URI,
+        // HTTP headers
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: `${process.env.GATSBY_BRIKL_USER_ID}`,
+          "x-brikl-shop-id": process.env.GATSBY_BRIKL_SHOP_ID,
+          "x-brikl-client-id": process.env.GATSBY_BRIKL_CLIENT_ID,
+        },
+        createSchema: async () => {
+          const sdl = fs
+            .readFileSync(`${__dirname}/brikl-schema.gql`)
+            .toString()
+          return buildSchema(sdl)
+        },
+        // Additional options to pass to node-fetch
+        // fetchOptions: {},
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "data",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-yaml",
+      typeName: "Shop",
+    },
+    {
+      resolve: "gatsby-plugin-material-ui",
+    },
+    {
+      resolve: "gatsby-plugin-typescript",
+    },
+    {
+      resolve: "gatsby-plugin-react-helmet",
+    },
+  ],
 }
