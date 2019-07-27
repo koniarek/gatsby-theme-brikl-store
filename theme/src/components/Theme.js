8a5acbb05bfc27eba0aa8c73f98feb7b14dac3b1
Patrik Molnar <info@patrikmolnar.com>
2019-07-27 13:33:54 +0700

    setup material ui theming

---
+++ theme/src/components/Theme.js
@@ -0,0 +1,20 @@
+import { createMuiTheme } from "@material-ui/core/styles"
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#8a4baf",
+      contrastText: "#fff",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#ffcc00",
+    },
+    // error: will use the default color
+  },
+})
