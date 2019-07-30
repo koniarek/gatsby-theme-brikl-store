992c9e9e77c1bdcbabd8b131b563c55038086964
Patrik Molnar <info@patrikmolnar.com>
2019-07-30 17:55:31 +0700

    products page cards fix

--- theme/src/components/FeaturedProducts.js
+++ theme/src/components/FeaturedProducts.js
@@ -59,55 +59,63 @@ const FeaturedProducts = ({ products, urlPrefix, langCode }) => {
                   }}
                 >
                   <CardActionArea
-                  style={{
-                    height: "100%",
-                  }}
+                    style={{
+                      height: "100%",
+                    }}
                   >
-                  <Grid container alignItems='space-between' style={{height: '100%', paddingBottom: 16}}>
-                    <Grid item xs={12}>
-                    <img
-                      src={product.media[0].image}
-                      style={{
-                        height: 200,
-                        width: "100%",
-                        objectFit: "contain",
-                      }}
-                      alt="Thumbnail"
-                    />
-                    </Grid>
-<Grid item xs={12}>
-                      <Typography
-                        style={{ padding: '8px 16px' }}
-                        gutterBottom
-                        variant="h5"
-                        component="h2"
-                      >
-                        {getTextFromLanguage(product.title.text, langCode)}
-                      </Typography>
+                    <Grid
+                      container
+                      alignItems="space-between"
+                      style={{ height: "100%", paddingBottom: 16 }}
+                    >
+                      <Grid item xs={12}>
+                        <img
+                          src={product.media[0].image}
+                          style={{
+                            height: 200,
+                            width: "100%",
+                            objectFit: "contain",
+                          }}
+                          alt="Thumbnail"
+                        />
                       </Grid>
                       <Grid item xs={12}>
-                      <Typography
-                        style={{ padding: '8px 16px' }}
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {getTextFromLanguage(
-                          product.description.text,
-                          langCode
-                        )}
-                      </Typography>
+                        <Typography
+                          style={{ padding: "8px 16px" }}
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                        >
+                          {getTextFromLanguage(product.title.text, langCode)}
+                        </Typography>
                       </Grid>
                       <Grid item xs={12}>
-                      <Grid container alignItems='flex-end' style={{height: '100%'}}>
-                      <Typography
-                        style={{ padding: '8px 16px' }}
-                        variant="h5"
-                        component="h2"
-                      >
-                        ${product.price.value} {product.price.currency}
-                      </Typography>
+                        <Typography
+                          style={{ padding: "8px 16px" }}
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {getTextFromLanguage(
+                            product.description.text,
+                            langCode
+                          )}
+                        </Typography>
                       </Grid>
+                      <Grid item xs={12}>
+                        <Grid
+                          container
+                          alignItems="flex-end"
+                          style={{ height: "100%" }}
+                        >
+                          <Typography
+                            style={{ padding: "8px 16px" }}
+                            variant="h5"
+                            component="h2"
+                          >
+                            ${product.price.value} {product.price.currency}
+                          </Typography>
+                        </Grid>
                       </Grid>
                     </Grid>
                   </CardActionArea>
--- theme/src/components/ProductList.js
+++ theme/src/components/ProductList.js
@@ -45,7 +45,7 @@ const ProductList = ({ products, langCode, urlPrefix }) => {
           let product = productNode.node
 
           return (
-            <Grid item xs={10} sm={6} md={4} lg={3} xl={3} key={product.id}>
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={3} key={product.id}>
               <Link
                 to={
                   `${urlPrefix}/products/` +
@@ -56,49 +56,68 @@ const ProductList = ({ products, langCode, urlPrefix }) => {
                 <Card
                   style={{
                     height: "100%",
-                    padding: "0px 5px 0px 5px",
-                    margin: "0px 5px 0px 5px",
                   }}
                 >
-                  <CardActionArea>
-                    <img
-                      src={product.media[0].image}
-                      style={{
-                        height: 200,
-                        width: "100%",
-                        objectFit: "contain",
-                      }}
-                      alt="Thumbnail"
-                    />
-
-                    <CardContent>
-                      <Typography
-                        style={{ padding: 5 }}
-                        gutterBottom
-                        variant="h5"
-                        component="h2"
-                      >
-                        {getTextFromLanguage(product.title.text, langCode)}
-                      </Typography>
-                      <Typography
-                        style={{ padding: 5 }}
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {getTextFromLanguage(
-                          product.description.text,
-                          langCode
-                        )}
-                      </Typography>
-                      <Typography
-                        style={{ padding: 5 }}
-                        variant="h5"
-                        component="h2"
-                      >
-                        ${product.price.value} {product.price.currency}
-                      </Typography>
-                    </CardContent>
+                  <CardActionArea
+                    style={{
+                      height: "100%",
+                    }}
+                  >
+                    <Grid
+                      container
+                      alignItems="space-between"
+                      style={{ height: "100%", paddingBottom: 16 }}
+                    >
+                      <Grid item xs={12}>
+                        <img
+                          src={product.media[0].image}
+                          style={{
+                            height: 200,
+                            width: "100%",
+                            objectFit: "contain",
+                          }}
+                          alt="Thumbnail"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography
+                          style={{ padding: "8px 16px" }}
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                        >
+                          {getTextFromLanguage(product.title.text, langCode)}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography
+                          style={{ padding: "8px 16px" }}
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {getTextFromLanguage(
+                            product.description.text,
+                            langCode
+                          )}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Grid
+                          container
+                          alignItems="flex-end"
+                          style={{ height: "100%" }}
+                        >
+                          <Typography
+                            style={{ padding: "8px 16px" }}
+                            variant="h5"
+                            component="h2"
+                          >
+                            ${product.price.value} {product.price.currency}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Grid>
                   </CardActionArea>
                 </Card>
               </Link>
