b1aacb4dc641bc3298ea6d77aad4d4711d542f83
Patrik Molnar <info@patrikmolnar.com>
2019-07-31 11:19:15 +0700

    test shadowing

---
+++ demo/src/gatsby-theme-brikl-store/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Footer from "gatsby-theme-brikl-store/src/components/Footer"
+
+export const Test = () => (
+  <>
+    <h1>testing shadowing</h1>
+    <Footer />
+  </>
+)
--- theme/src/components/LangSwitch.js
+++ theme/src/components/LangSwitch.js
@@ -1,41 +1,48 @@
 import React from "react"
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from "@material-ui/core/styles"
 
-const useStyles = makeStyles((theme) =>
-createStyles({
-  select:{
-    '&:hover':{
-      cursor: 'pointer'
-    }
-  },
-}),
-);
+const useStyles = makeStyles(theme =>
+  createStyles({
+    select: {
+      "&:hover": {
+        cursor: "pointer",
+      },
+    },
+  })
+)
 const LangSwitch = ({ languages, setLanguage, selectedLanguage }) => {
   console.log(selectedLanguage)
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
-    <div style={{display: 'flex', alignItems:'center'}} className={classes.select}>
-    <select
-      value={selectedLanguage}
-      onChange={e => setLanguage(e.target.value)}
-      aria-label="language"
-      className={classes.selectApperance}
-      style={{
-        fontFamily: 'Roboto',
-        fontSize: 16,
-        paddingRight: 20,
-        border: 'none',
-        background:'transparent',
-     padding: 5,
-      }}
+    <div
+      style={{ display: "flex", alignItems: "center" }}
+      className={classes.select}
     >
-      {languages.map((lang, index) => (
-        <option value={lang.split("_")[0]} key={index} label={lang.split("_")[0] === 'en' ? 'English' : 'Français'}>
-          {lang.split("_")[0]}
-        </option>
-      ))}
-    </select>
+      <select
+        value={selectedLanguage}
+        onChange={e => setLanguage(e.target.value)}
+        aria-label="language"
+        className={classes.selectApperance}
+        style={{
+          fontFamily: "Roboto",
+          fontSize: 16,
+          paddingRight: 20,
+          border: "none",
+          background: "transparent",
+          padding: 5,
+        }}
+      >
+        {languages.map((lang, index) => (
+          <option
+            value={lang.split("_")[0]}
+            key={index}
+            label={lang.split("_")[0] === "en" ? "English" : "Français"}
+          >
+            {lang.split("_")[0]}
+          </option>
+        ))}
+      </select>
     </div>
   )
 }
