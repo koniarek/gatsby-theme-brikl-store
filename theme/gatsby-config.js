2b095048a59941d20ea358e13b450ec4acd9f7f9
Tobias Meixner <meixner.tobias@gmail.com>
2019-07-31 11:49:28 +0800

    pass theme options into theme

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -2,58 +2,61 @@ const env = require("dotenv").config()
 const fs = require("fs")
 const { buildSchema } = require("graphql")
 
-module.exports = {
-  siteMetadata: {
-    title: "Gatsby Theme BrikL Store",
-    titleTemplate: "%s · Gatsby Theme BrikL Store",
-    description: "A starter template for BrikL storefronts",
-    siteUrl: "http://localhost:8000",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "Brikl",
-        fieldName: "brikl",
-        // Url to query from
-        url: process.env.GATSBY_BRIKL_API_URI,
-        // HTTP headers
-        headers: {
-          // Learn about environment variables: https://gatsby.dev/env-vars
-          Authorization: `${process.env.GATSBY_BRIKL_USER_ID}`,
-          "x-brikl-shop-id": process.env.GATSBY_BRIKL_SHOP_ID,
-          "x-brikl-client-id": process.env.GATSBY_BRIKL_CLIENT_ID,
+module.exports = themeOptions => {
+  const { clientId, userId, shopId } = themeOptions
+  return {
+    siteMetadata: {
+      title: "Gatsby Theme BrikL Store",
+      titleTemplate: "%s · Gatsby Theme BrikL Store",
+      description: "A starter template for BrikL storefronts",
+      siteUrl: "http://localhost:8000",
+    },
+    plugins: [
+      {
+        resolve: "gatsby-source-graphql",
+        options: {
+          typeName: "Brikl",
+          fieldName: "brikl",
+          // Url to query from
+          url: "https://api.mybrikl.com/graphql",
+          // HTTP headers
+          headers: {
+            // Learn about environment variables: https://gatsby.dev/env-vars
+            Authorization: userId,
+            "x-brikl-shop-id": shopId,
+            "x-brikl-client-id": clientId,
+          },
+          createSchema: async () => {
+            const sdl = fs
+              .readFileSync(`${__dirname}/brikl-schema.gql`)
+              .toString()
+            return buildSchema(sdl)
+          },
+          // Additional options to pass to node-fetch
+          // fetchOptions: {},
         },
-        createSchema: async () => {
-          const sdl = fs
-            .readFileSync(`${__dirname}/brikl-schema.gql`)
-            .toString()
-          return buildSchema(sdl)
+      },
+      {
+        resolve: "gatsby-plugin-material-ui",
+      },
+      {
+        resolve: "gatsby-plugin-typescript",
+      },
+      {
+        resolve: "gatsby-plugin-react-helmet",
+      },
+      {
+        resolve: "gatsby-plugin-html-attributes",
+        options: {
+          lang: "en",
         },
-        // Additional options to pass to node-fetch
-        // fetchOptions: {},
       },
-    },
-    {
-      resolve: "gatsby-plugin-material-ui",
-    },
-    {
-      resolve: "gatsby-plugin-typescript",
-    },
-    {
-      resolve: "gatsby-plugin-react-helmet",
-    },
-    {
-      resolve: "gatsby-plugin-html-attributes",
-      options: {
-        lang: "en",
+      {
+        resolve: "gatsby-plugin-robots-txt",
+        options: {
+          policy: [{ userAgent: "*", allow: "/" }],
+        },
       },
-    },
-    {
-      resolve: "gatsby-plugin-robots-txt",
-      options: {
-        policy: [{ userAgent: "*", allow: "/" }],
-      },
-    },
-  ],
+    ],
+  }
 }
