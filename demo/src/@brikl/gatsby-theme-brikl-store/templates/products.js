3d4d28004efc2af610c32ce4ce3b634fe43ae410
Patrik Molnar <info@patrikmolnar.com>
2019-07-31 15:19:50 +0700

    test shadow

--- demo/src/@brikl/gatsby-theme-brikl-store/templates/products.js
+++ demo/src/@brikl/gatsby-theme-brikl-store/templates/products.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ProductList from "@brikl/gatsby-theme-brikl-store/src/components/ProductList"
 import Layout from "../components/Layout"
+import SEO from "@brikl/gatsby-theme-brikl-store/src/components/SEO"
 
 const ProductsPage = ({ pageContext }) => {
   const products = pageContext.shop.products.edges
@@ -10,6 +11,7 @@ const ProductsPage = ({ pageContext }) => {
   return (
     <Layout pageContext={pageContext}>
       <h1>shadow</h1>
+      <SEO title="Products" />
       <ProductList
         langCode={langCode}
         urlPrefix={urlPrefix}
